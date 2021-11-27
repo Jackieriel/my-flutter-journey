991fd6bdfe96b0b7f91397d7be481e7489c00d39
jackieriel <jonahjacksonj@gmail.com>
2021-11-27 11:01:23 +0100

    I am rich app

---
+++ android/app/src/main/kotlin/com/jackieriel/mysms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jackieriel.mysms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My SMS APP'),
+        ),
+        body:
+            Center(child: Text('Welcome to my SMS app, this is just testing')),
+      ),
+    ),
+  );
+}
